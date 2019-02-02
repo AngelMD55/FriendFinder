3806d96502e647b8af4bffe9d114fc59e48cbf1b
AngelMD55 <angel_demerutis@hotmail.com>
2019-02-02 14:08:29 -0700

    final draft

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,12 +8,12 @@ app.get("/api/friends", function(req, res){
 // /api/friends --- POST to handle incoming survey form
 app.post("/api/friends", function(req, res){
     let newFriend = (req.body);
-    for(let i = 0; i< (newFriend.score).length; i++){
-        console.log(parseInt(newFriend.score[i]));
-    }
+    // for(let i = 0; i< (newFriend.score).length; i++){
+    //     console.log(parseInt(newFriend.score[i]));
+    // }
 
     friends.push(newFriend);
-    res.json(newFriend);
-    res.status(200).json({sucess:"IM BEING SENT BACK"})
+    res.status(200).json(newFriend);
+    // res.status(200).json({sucess:"IM BEING SENT BACK"})
 });
 }
